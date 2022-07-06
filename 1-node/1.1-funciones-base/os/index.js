18ee0d4d67ed2c33647eacf7830ba4625c2d40ca
amondrag <amondrag@comunidad.unam.mx>
2022-07-05 21:32:58 -0500

    Cambios de la clase

---
+++ 1-node/1.1-funciones-base/os/index.js
@@ -0,0 +1,11 @@
+const os = require('os');
+
+console.log(os.version());
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.freemem());
+console.log(os.homedir());
+console.log('IP: ' , os.hostname());
+console.log(os.platform());
+console.log(os.version());
+console.log();
